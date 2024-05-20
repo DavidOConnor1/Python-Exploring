b16e591d622c7de1d32586d5def73aba1796ef93
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 16:32:30 +0100

    learning math functions

---
+++ Number Functions/MathFunctions.py
@@ -0,0 +1,10 @@
+import math
+pi = 3.14
+
+print(round(pi)) # it will round to the nearet full integer value
+
+print(math.ceil(pi)) #it will round to the next full integer value
+
+print(math.floor(pi)) # it will round down to the next full integer value
+
+print(abs(pi)) # it will return the absolute value of the number
