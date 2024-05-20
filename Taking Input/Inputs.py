4ef118232445f749544416e709e40a3302973aa7
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 16:16:20 +0100

    Mixing my casting with the inputs

--- Taking Input/Inputs.py
+++ Taking Input/Inputs.py
@@ -1,1 +1,11 @@
-name = input("What is your name? ")
+name = input("What is your name? ") # always take input as string 
+age = int(input("what is your age? ")) # always take input as integer
+#dvahuman = input("Are you a human? ")
+
+#name = str(name)
+age +=1
+age = str(age)
+
+
+print("Hello "+name)
+print("Happy Birthday! You are now "+age+" years old!")
