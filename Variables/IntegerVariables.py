932fff6c5e64c4611fd91a6f95683990d36fab4b
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 14:43:00 +0100

    learning about casting in python

--- Variables/IntegerVariables.py
+++ Variables/IntegerVariables.py
@@ -4,4 +4,11 @@ age = 20
 age += 1
 
 print(age)
-print(type(age))
+
+#print(type(age))
+
+# You cannot have a int variable mixed with a string statement in a print line because they ae not the same data type
+
+#You have to cast the int variable to a string variable to be able to print them together
+
+print("Your Age is "+str(age))
