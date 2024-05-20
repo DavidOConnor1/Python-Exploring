3e84bf107c9a6599abb16336a435a87f11bd2d55
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 16:02:49 +0100

    Learning more about type casts

--- Type Cast/TypeCasting.py
+++ Type Cast/TypeCasting.py
@@ -19,3 +19,27 @@ print(z)
 #z after type casting to int with a multiple of 2
 
 print(z*2)
+
+print("\nfloat type casting")
+
+xx = float(x)
+yy = float(y)
+zz = float(z)
+
+print(xx)
+print(yy)
+print(zz)
+print(zz*2)
+
+#string type casting
+
+print("\nString type casting")
+
+xxy = str(x)
+yyz = str(y)
+zzx = str(z)
+
+print(xxy)
+print(yyz)
+print(zzx)
+print(zzx*2)
