9c78abb424655b161a4a86baaa6e411998a205ce
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 16:20:34 +0100

    having fiun with the inputs

--- Taking Input/Inputs.py
+++ Taking Input/Inputs.py
@@ -1,6 +1,6 @@
 name = input("What is your name? ") # always take input as string 
 age = int(input("what is your age? ")) # always take input as integer
-#dvahuman = input("Are you a human? ")
+height = float(input("what is your height? ")) # always take input as float
 
 #name = str(name)
 age +=1
@@ -9,3 +9,4 @@ age = str(age)
 
 print("Hello "+name)
 print("Happy Birthday! You are now "+age+" years old!")
+print("you are " +str(height)+"cm taller")
