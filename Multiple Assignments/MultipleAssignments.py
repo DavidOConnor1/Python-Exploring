d4491387e00f3fca83fb8f15ca1aa4e97240ac3b
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 15:04:52 +0100

    I have learnt how to use multiple assignment in python

---
+++ Multiple Assignments/MultipleAssignments.py
@@ -0,0 +1,27 @@
+# multiple assignments = allows us to assign various amount of variables at the same time with one line of code
+
+#example of without multiple assignments
+name = "Bro"
+age = 25
+attractive = True
+
+print(name)
+print(age)
+print(attractive)
+
+#example of with multiple assignments
+
+name1, age1, attractive1 = "Kurt", 27, True
+
+print(name1)
+print(age1)
+print(attractive1)
+
+# multiple assignments with the same value
+
+luffy = nami=  zoro = usopp = 20
+
+print(luffy)
+print(nami)
+print(zoro)
+print(usopp)
