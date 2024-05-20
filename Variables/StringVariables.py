de8b72a2c47ab08dc6675c83ab5ecb240e85ee8c
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 14:35:47 +0100

    Learning varibles in python

---
+++ Variables/StringVariables.py
@@ -0,0 +1,11 @@
+# variables are a container for a value and behaves the as the value that it contains
+
+#type wuill return the type of the variable
+
+# String Variables
+first_name = "Rachel"
+last_name = "Green"
+
+full_name = first_name+ " " +last_name
+
+print("Hello "+full_name)
