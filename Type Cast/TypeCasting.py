85c2424f78fb88f770fce687430dbee4a2293032
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 15:58:05 +0100

    int type casting

---
+++ Type Cast/TypeCasting.py
@@ -0,0 +1,21 @@
+# type casting is to convert a data type of a value to another data type
+
+
+
+x = 1 #int
+y = 2.0 #float ]
+z = "3" # String
+
+#casting other values to int
+
+y = int(y)
+z = int(z)
+
+print("int type casting")
+print(x)
+print(y)
+print(z)
+
+#z after type casting to int with a multiple of 2
+
+print(z*2)
