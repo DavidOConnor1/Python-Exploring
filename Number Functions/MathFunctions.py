13d10e2dd8d179eeb862d308a551c10ad0f9f4ee
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-20 16:36:50 +0100

    all of the basic math functions

--- Number Functions/MathFunctions.py
+++ Number Functions/MathFunctions.py
@@ -1,5 +1,8 @@
 import math
 pi = 3.14
+x =1
+y= 4
+z = 3
 
 print(round(pi)) # it will round to the nearet full integer value
 
@@ -8,3 +11,11 @@ print(math.ceil(pi)) #it will round to the next full integer value
 print(math.floor(pi)) # it will round down to the next full integer value
 
 print(abs(pi)) # it will return the absolute value of the number
+
+print(pow(pi,2)) # will raise a base number to a power
+
+print(math.sqrt(pi)) # will return the squre root of the number
+
+print(max(x,y,z)) # will return the maximum value
+
+print(min(x,y,z)) # will return the minimum value
