5ec997afe8934141af597bb4a4c908bfcc94096b
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 13:19:35 +0100

    Made stairs with nested loops

--- Nested Loops/NestedLoops.py
+++ Nested Loops/NestedLoops.py
@@ -1,11 +1,23 @@
 #nested loops are the inner loops that will finish all of iterations before
 #before finishing the outer loops
 
-rows = int(input("how many rows?: "))
-columns = int(input("How many columns?: "))
-symbol = input(("enter your symbol: "))
+#rows = int(input("how many rows?: "))
+#columns = int(input("How many columns?: "))
+#symbol = input(("enter your symbol: "))
 
+#for i in range(rows):
+ #   for j in range(columns):
+ #       print(symbol, end="")
+  #  print()
+
+print("Lets Make Stairs")
+   
+rows = 5
+columns = 1
+symbol ="*"   
+    
 for i in range(rows):
     for j in range(columns):
         print(symbol, end="")
+        columns+=1
     print()
