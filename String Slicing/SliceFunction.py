a41148203e6ab0e3677cd8608bcb25a7dbcf59aa
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 12:00:26 +0100

    Python is fun

---
+++ String Slicing/SliceFunction.py
@@ -0,0 +1,13 @@
+website = "http://google.com"
+
+slice = slice(7,-4)
+
+website = website[slice]
+
+print(website)
+
+website2 = "http://wikipedia.com"
+
+website2 = website2[slice]
+
+print(website2)
