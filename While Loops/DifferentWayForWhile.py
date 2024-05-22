1d352844c3f5816da9656742de0cc8cd82c8d214
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 12:50:32 +0100

    Whle loops suck

---
+++ While Loops/DifferentWayForWhile.py
@@ -0,0 +1,6 @@
+name = None
+
+while not name:
+    name = input("Enter the name: ")
+    
+print("Hello "+name)
---
+++ While Loops/While.py
@@ -0,0 +1,9 @@
+#while is a statement that will execute its block of code as long as the condtion remains true
+
+name = ""
+
+while len(name) == 0:
+    name = input("what is your name: ")
+    
+    
+print("Hello "+name)
