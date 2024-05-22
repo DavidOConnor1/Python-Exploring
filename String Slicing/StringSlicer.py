128d4cd099f36f4c380b165cd7453a0e2a516c3a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 11:39:06 +0100

    Learning slice is a fun function

---
+++ String Slicing/StringSlicer.py
@@ -0,0 +1,14 @@
+#slicing = create a sub string by extrqacting elements from another string
+# indexing = extract an individual element from a string
+#slicing have three optional arguments start:stop:step
+
+name = "Monkey D. Luffy"
+
+#first_name = name[0:6]
+first_name = name[:6] #short cut way of writing the above line
+
+#Last_name = name[7:15]
+last_name = name[7:] #short cut way of writing the above line
+
+print(first_name)
+print(last_name)
