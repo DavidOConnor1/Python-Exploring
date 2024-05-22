017e66ba5c87be32c3815918808408ce12ef420f
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 13:01:51 +0100

    For loops are way more simple in python

--- For Loops/ForLooping.py
+++ For Loops/ForLooping.py
@@ -1,3 +1,4 @@
+import time
 #for loop will execute a limited amount of times
 
 
@@ -5,6 +6,14 @@
 #for i in range(10):
     #print(i+1)
  
-for i in range(20,120+1,2):
-    print(i)   
+#for i in range(20,120+1,2):
+    #print(i)
+    
+#for i in "One Piece":
+    #print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("We found the one piece")
     
