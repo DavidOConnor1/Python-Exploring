82e7f554a6df886252de112d614592f164db58e4
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-22 12:21:49 +0100

    nice operators

---
+++ Logical Operators/AndORLogical.py
@@ -0,0 +1,8 @@
+temp = int(input("How hot is it outside?: "))
+
+if temp>= 0 and temp<=30:
+    print("it is okay today")
+    print("Enjoy the sun")
+elif temp<0 or temp> 30:
+    print("the temp sucks today")
+    print("order uber eats")
