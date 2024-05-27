010ce0a5f3f71c173c74c695fb3170e0da16589e
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 16:14:09 +0100

    Refactor Sets.py to use union method for creating a new set

--- Sets/Sets.py
+++ Sets/Sets.py
@@ -1,10 +1,21 @@
 # set = collection which is unordered, unindexed and no duplicate values
 
 utentsils = {"fork", "spoon", "knife"}
+dishes = {"plate", "bowl", "mug"}
 
+#you can do this either way you want
+#utentsils.update(dishes)
+
+#like a standard list
 #utentsils.add("hammer")
 #utentsils.remove("fork")
 #utentsils.clear()
 
-for x in utentsils:
-    print(x)
+#creating a new set
+dinner_table = utentsils.union(dishes)
+
+#for x in utentsils:
+ #   print(x)
+  
+#for i in dinner_table:
+ #   print(i)  
