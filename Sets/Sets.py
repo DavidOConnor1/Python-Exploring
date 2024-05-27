37bbaa4d423ad7cf8cefffb406ef03e2b7e01208
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 16:08:37 +0100

    sets are fun

---
+++ Sets/Sets.py
@@ -0,0 +1,10 @@
+# set = collection which is unordered, unindexed and no duplicate values
+
+utentsils = {"fork", "spoon", "knife"}
+
+#utentsils.add("hammer")
+#utentsils.remove("fork")
+#utentsils.clear()
+
+for x in utentsils:
+    print(x)
