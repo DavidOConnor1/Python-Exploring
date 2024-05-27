911be17cafffa9462c460e6b817f41c655b0cf3e
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 15:30:38 +0100

    finished writing loop control statements

---
+++ Break_Continue_Pass/LoopControlStatements.py
@@ -0,0 +1,22 @@
+# break to terminate the loop entrierly
+#continue skips to the next iteration of the loop
+#pass = does nothing acts as a placeholder
+
+#while True:
+ #   name = input("Enter your name!")
+ #   if name != "":
+#        break
+    
+
+#phone_number = "083-555-555"
+
+#for i in phone_number:
+ #   if i == "-":
+  #      continue
+  #  print(i, end="")
+  
+for i in range(1,21):
+    if i == 13:
+        pass
+    else:
+        print(i)
