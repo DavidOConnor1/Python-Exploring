873b8d88dcad8801e4b413819d7d9928f7ff5e61
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 16:53:13 +0100

    making sub strings using indexing. Indexing is reminding of the slice function from last week

---
+++ Indexing/Indexing.py
@@ -0,0 +1,12 @@
+# index operator [] gives access to the sequence of the element include but not limited to (str, list, tuples)
+
+name = "monkey d. Luffy"
+
+if(name[0].islower()):
+    name = name.capitalize()
+    
+first_name = name[:6].upper()
+
+#print(name)
+print(first_name)
+
