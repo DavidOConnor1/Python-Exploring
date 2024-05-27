af5b3a9632cbf917bcbec9f8d040a80f06dec70a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 17:11:39 +0100

    functions are even simplifed

---
+++ Functions/Functions.py
@@ -0,0 +1,11 @@
+#a block of code that only runs when it is called
+
+def hello(name):
+    print("hello "+name)
+    print("have a nice day")
+    
+#hello()
+
+#calling the hello function 3 times
+for i in range(3):
+    hello("davey")
