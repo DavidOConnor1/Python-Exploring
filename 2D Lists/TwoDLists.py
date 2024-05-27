5c6f0c7015d522561079a123da7c72690b838d80
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 15:51:53 +0100

    learned some 2d list stuff. I am happy with my progress in regards of learning python

---
+++ 2D Lists/TwoDLists.py
@@ -0,0 +1,10 @@
+gopro =["Cameras", "goPro_camera"]
+helment =["white helment", "black helment"]
+quality = ["4K", "1440p", "1080p"]
+
+adventure = [gopro, helment, quality]
+
+for i in adventure:
+    print(i)
+    
+print(adventure[0][1])
