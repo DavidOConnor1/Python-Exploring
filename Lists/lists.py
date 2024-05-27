8ad5835d19d054e10718f0042021f7594aae617a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 15:43:03 +0100

    python ,akes lists too easy

---
+++ Lists/lists.py
@@ -0,0 +1,15 @@
+# lists = used to store multiple items
+
+food = ["potatoes", "pizza", "flakes"]
+
+food[0] = "muffin"
+#food.append("ice cream")
+#food.remove("pizza")
+#food.pop()
+#food.insert(0,"cake")
+#food.sort()
+food.clear()
+
+
+for i in food:
+    print(i)
