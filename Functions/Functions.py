22cd18ef6946639b6489db5a0bebffddffa2c09b
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 17:15:21 +0100

    feat: Refactor hello function to capitalize the first letter of the name

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,11 +1,14 @@
 #a block of code that only runs when it is called
 
 def hello(name):
-    print("hello "+name)
+    print("Hello "+name)
     print("have a nice day")
     
 #hello()
 
 #calling the hello function 3 times
-for i in range(3):
-    hello("davey")
+#for i in range(3):
+ #   hello("davey")
+
+#hello(input("Enter Your Name: ")) #Taking input from the user when using the hello function since input is automatically assigned to string
+
