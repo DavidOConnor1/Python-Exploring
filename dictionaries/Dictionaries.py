abaa46f38a1668cdb1db98e629dfd64090f321ad
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-05-27 16:27:24 +0100

    learning dictonaries in py

---
+++ dictionaries/Dictionaries.py
@@ -0,0 +1,9 @@
+#changeable unordered collection of unique key: value pairs
+# They are fast because they use hashing, they allow quick access a value quickly
+
+captials = {'Ireland': 'Dublin',
+            'USA': 'Washington',
+            'India': 'Dehli',
+            'Germany': 'Berlin',
+            'Russia': 'Moscow'}
+
