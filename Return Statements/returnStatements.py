2496da69d8e88c05c2269b2df3e2fdead814def5
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-06-05 13:45:44 +0100

    I have used the return statement in py

---
+++ Return Statements/returnStatements.py
@@ -0,0 +1,9 @@
+#return statement = functions sned python values or object back to the caller 
+# these values/objects are know as the functions return value
+
+def multiply(num1, num2):
+    return num1 * num2
+
+x = multiply(2,2)
+
+print("The answer from the multiplication method is: "+str(x))
